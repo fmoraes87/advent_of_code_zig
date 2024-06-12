d74fc0617e19d4c33fcfddcb2ee2900928b619ad
FernandoM <m87.fernando@gmail.com>
2024-06-12 08:19:31 -0400

    feat: use std.plit to parse lines

--- day2/challenge_p1.zig
+++ day2/challenge_p1.zig
@@ -9,7 +9,18 @@ pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
 
     var parcial_sum: i32 = 0;
-    while (try f.reader().readUntilDelimiterOrEof(&buf, '\n')) |c| {}
+    while (try f.reader().readUntilDelimiterOrEof(&buf, '\n')) |game| {
+        var game_parts = std.mem.split(u8, game, ":");
+        var game_id = game_parts.next().?;
+        try stdout.print("{s}\n", .{game_id});
+        var subset_cubes = std.mem.split(u8, game_parts.next().?, ";");
+        while (subset_cubes.next()) |bags| {
+            var cubes = std.mem.split(u8, bags, ",");
+            while (cubes.next()) |cube| {
+                try stdout.print("Cube {s}\n", .{cube});
+            }
+        }
+    }
 
     try stdout.print("Answer {}", .{parcial_sum});
 }
